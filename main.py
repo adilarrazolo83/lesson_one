dbf87538eaaac163f11f4954e2fa012675f36d63
Adi Larrazolo <larrazoloadi@gmail.com>
2021-02-21 12:57:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+my_message = "Hello World"
+print(my_message)
+
+animal_type = "cat"
+print(animal_type)
+
+human_age = 34
+print(human_age)
+
+weather = "bright and sunny"
+print(weather)
+
+my_message = "Hello World, how are you?"
+print(my_message)
+
+this_message = my_message
+
+my_message = "\tI should be shifted to the right"
+print(my_message)
+
+my_message = "Im on the line \nbut I'm on the next line"
+print(my_message)
+
+my_text = "She said\"Hello!\""
+print(my_text)
+
+add_two_numbers = 2+2
+print(add_two_numbers)
+
+# 1. Within the main.py file, create three variables that are set to the following questions:
+question_one = "What is your name?"
+print(question_one)
+
+question_two = "How old are you?"
+print(question_two)
+
+question_three = "How many months is that?"
+print(question_three)
+
+
+# 2. Next, create three more variables that are set to the following answers to the above questions:
+answer_one = 'My name is "Billy"'
+print(answer_one)
+
+answer_two = 30
+print(answer_two)
+
+answer_three = 360
+print(answer_three)
+
+# 3. Use the print function to print each of the variables. 
+print(question_one)
+print(answer_one)
+
+print(question_two)
+print(answer_two)
+
+print(question_three)
+print(answer_three) 
+
+# 4. Next, comment out the appropriate variables and print functions so the output now looks like below:
+print(question_one)
+print(answer_one)
+
+print(question_two)
+print(answer_two)
+
+#print(question_three)
+#print(answer_three) 
+
+# 5. Finally, create three new variables: my_string, my_integer, and my_float.
+#Each variable should be set to the appropriate data type based on the name of the variable name. The actual values you use is not important. What is important is that you match up the right type of data with the right variable.
+#Print out each variable on separate lines.
+my_string = "I love to code"
+print(my_string)
+
+my_integer = 365
+print(my_integer)
+
+my_float = 10,5
+print(my_float)
+
+
+
